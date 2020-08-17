3f9ee994bab1abaa5b002437062501cba1f6f571
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2020-08-17 23:29:17 +0200

    Create config.lua

---
+++ lua/configuration/config.lua
@@ -0,0 +1,132 @@
+local cfg = Coinflip:CreateConfig()
+-- This is how fast the coin should be spinning at the start. 
+-- I recommend 18, but you shouldn't really put it below 12.
+cfg:SetSpeed(18)
+
+-- What colour should the prefix be?
+-- You can use Color(r, g, b)
+cfg:SetChatPrefixColor(XeninUI.Theme.Accent)
+
+-- What text should the prefix for chat messages?
+cfg:SetChatPrefix("[COINFLIP] ")
+
+-- What is the minimum amount someone can bet?
+cfg:SetMinBet(100)
+
+-- What is the maximum amount someone can bet?
+cfg:SetMaxBet(1000000)
+
+-- How many coinflips can a user have active at once?
+cfg:SetMaxCoinflips(3)
+
+-- Xenin Coinflip - VIP System by dotCore (14.08 update)
+
+--[[
+	Hello there !
+	Here's the files that should be replaced : config.lua / config_wrapper.lua / server.lua / games.lua
+
+	Because Xenin is Xenin, I was too lazy to create a VIP Ranks table
+	You can wait for the official integration by Xenin Team if thats coming one day 
+
+	Or you can go to lua/coinflip/networking/server.lua & lua/coinflip/ui/games.lua and do :
+		server.lua : edit line 44 and 49 (the two 'if' statements)
+		games.lua : edit line 48 / 62 / 82 / 91 (the four 'if' statements)
+	Thats it !
+
+	You can also try from your own to create a table
+	That should not be that hard, except if you want to go through their config_wrapper
+
+	Thanks for using this and good game ! (hope you will earn a lot of money thanks to this)
+]]
+
+-- How many coinflips can VIP users have active at once?
+cfg:SetMaxVIPCoinflips(6)
+
+-- What should it say in the top left of the menu?
+cfg:SetMenuTitle("Waurum - Coinflips")
+
+-- What text should be in the overhead of the NPC?
+cfg:SetNPCTitle("Coinflip NPC")
+
+-- What model should the NPC have?
+cfg:SetNPCModel("models/Barney.mdl")
+
+-- What icon should there be in the overhead of the NPC besides the text?
+-- Example: Material("xenin/coinflip/icon.png", "smooth")
+-- By default it's set to nil, which means it wont have an icon.
+cfg:SetNPCIcon(nil)
+
+-- What color should the NPC overhead have as an outline?
+cfg:SetNPCOutlineColor(Color(205, 50, 50))
+
+-- Disable the creation annoucement?
+cfg:SetCreationAnnouncementDisabled(false)
+
+-- Disable the win announcement?
+cfg:SetWinAnnouncementDisabled(true)
+
+-- Seed regularly
+-- Changes how the coinflip deals with randomness.
+-- Not really more random, just a different way
+cfg:SetSeedRegularly(false)
+
+-- What currency should coinflips pre v2 be?
+-- If you have never had a coinflip in your database before v2 this will do literally nothing.
+-- If not, make sure it's the right currency.
+cfg:SetOldCurrency("Pointshop 2")
+
+-- What currency?
+-- Supports:
+-- DarkRP
+-- Pointshop
+-- Pointshop 2
+------
+-- You can make this a table, i.e { "DarkRP", "Pointshop" } to have multiple currencies
+-- To make it a single currency, just have it "DarkRP"
+cfg:SetCurrency("Pointshop 2")
+
+/*
+	Set the language!
+	You can find the languages you can use here: https://gitlab.com/sleeppyy/xenin-languages/tree/master/coinflip
+	You dont need to write the .json part
+
+	If you want to add your own language you can
+	1. Create a pull request (create new file) that will be uploaded to that website with the language
+	2. Use a second argument in the :SetLanguage function
+
+	How to do now #2. This will set the language without needing to use the version from a website. 
+	cfg:SetLanguage("french", [[
+		{
+			"phrases": {
+				"ui": {
+					
+				}
+			}
+		}
+	]])
+
+	So for example
+	cfg:SetLanguage("russian", [[
+		-- copy the contents of english.json and translate it here
+	]])
+
+	Its recommended you use method #1, but you can use method #2 till the file you upload have been approved
+*/
+cfg:SetLanguage("English")
+
+-- What text messages should work as a way to open up the flip menu?
+-- If you don't want any chat commands you just remove every command
+cfg:AddChatCommand("!flip")
+cfg:AddChatCommand("/flip")
+cfg:AddChatCommand("!flips")
+cfg:AddChatCommand("/flips")
+cfg:AddChatCommand("!createflip")
+cfg:AddChatCommand("/createflip")
+cfg:AddChatCommand("!createflips")
+cfg:AddChatCommand("/createflips")
+cfg:AddChatCommand("!coinflip")
+cfg:AddChatCommand("/coinflip")
+cfg:AddChatCommand("!coinflips")
+cfg:AddChatCommand("/coinflips")
+cfg:AddChatCommand("!cf")
+cfg:AddChatCommand("/cf")
